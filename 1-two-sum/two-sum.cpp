3c2977f749a22b2a2896bb0a733cf15f27410a83
Conner Groth <124554088+connergroth@users.noreply.github.com>
2025-06-05 14:59:53 -0600

    Time: 3 ms (71.76%) | Memory: 14.8 MB (50.99%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // optimized 
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for(int i = 0; i < nums.size(); i++){
+            int diff = target - nums[i];
+            if(seen.count(diff)){
+                return{seen[diff], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+
+    /* 
+    // brute force 
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> targetNums;
         for(int i = 0; i < nums.size() - 1; i++){
@@ -12,4 +27,5 @@ public:
         }
         return targetNums;
     }
+    */ 
 };
