515b26a0eb6e76e44aac5eb4dd9cf2c3095244f1
Conner Groth <124554088+connergroth@users.noreply.github.com>
2025-10-09 21:43:14 -0600

    Time: 0 ms (100.00%) | Memory: 12.3 MB (84.27%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # rob1, rob2, n, n+1 ... 
+        rob1, rob2 = 0, 0
+
+        for num in nums:
+            temp = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
